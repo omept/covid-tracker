e2724a82a51a80f775bd0b2385cd2dfd056eb800
onwuasoanya_george <ndu4george@gmail.com>
2020-10-21 16:46:00 +0100

    add base structure for chart, cards and country picker

---
+++ src/components/Cards/Cards.jsx
@@ -0,0 +1,18 @@
+import { render } from '@testing-library/react';
+import React from 'react';
+
+// class Cards extends React.Component{
+//     render(){
+//         return (
+//             <h1>Card Here</h1>
+//         )
+//     }
+// }
+
+const Cards = () => {
+        return (
+            <h1>Cards</h1>
+        );
+}
+
+export default Cards;
