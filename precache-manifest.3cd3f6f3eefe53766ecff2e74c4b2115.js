f79bde95406ccc73506a276a537b652e555ef889
onwuasoanya_george <ndu4george@gmail.com>
2020-10-23 06:12:17 +0100

    Updates

---
+++ precache-manifest.3cd3f6f3eefe53766ecff2e74c4b2115.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "335fdc1513b9b29ee56dbda1c9c7e51c",
+    "url": "/covid-tracker/index.html"
+  },
+  {
+    "revision": "47807e08544675a5724a",
+    "url": "/covid-tracker/static/css/main.6628dc0b.chunk.css"
+  },
+  {
+    "revision": "0b8cca955bed5bb5b765",
+    "url": "/covid-tracker/static/js/2.6e4fe4e6.chunk.js"
+  },
+  {
+    "revision": "a07000321a1ebac9c07126414b759ee0",
+    "url": "/covid-tracker/static/js/2.6e4fe4e6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "47807e08544675a5724a",
+    "url": "/covid-tracker/static/js/main.719e963d.chunk.js"
+  },
+  {
+    "revision": "7d04b94fdcdbdec20f7c",
+    "url": "/covid-tracker/static/js/runtime-main.b2850d78.js"
+  },
+  {
+    "revision": "169be1043edd9d2df0e405d22e88f3c2",
+    "url": "/covid-tracker/static/media/coronavirus_logo.169be104.jpg"
+  }
+]);
